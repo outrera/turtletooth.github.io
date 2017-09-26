6656b21d0585a85da3c4623ca87b30170389bddc
Jeremy Bullock <jcbullock@gmail.com>
2017-09-26 00:01:20 -0600

    Basics in place

--- content/contact.md
+++ content/contact.md
@@ -5,7 +5,3 @@ id = "contact"
 
 # How can I help you?
 
-Are you curious about something? Do you have some kind of problem with an assignment or software?
-
-Please feel free to contact me, my Jupiter based customer service center is working for you 24/7.
-
--- themes/turtletooth/static/js/main.js
+++ themes/turtletooth/static/js/main.js
@@ -1,53 +1,8 @@
-$(function () {
-
-});
-
-$.fn.alignElementsSameHeight = function () {
-    $('.same-height-row').each(function () {
-
-  var maxHeight = 0;
-  var children = $(this).find('.same-height');
-  children.height('auto');
-  if ($(window).width() > 768) {
-      children.each(function () {
-    if ($(this).innerHeight() > maxHeight) {
-        maxHeight = $(this).innerHeight();
-    }
-      });
-      children.innerHeight(maxHeight);
-  }
-
-  maxHeight = 0;
-  children = $(this).find('.same-height-always');
-  children.height('auto');
-  children.each(function () {
-      if ($(this).height() > maxHeight) {
-    maxHeight = $(this).innerHeight();
-      }
-  });
-  children.innerHeight(maxHeight);
-
+$(document).ready(function () {
+    $(".youtube-link").click(function (event) {
+        $("#youtube_video").attr("src", "https://youtube.com/embed/" + event.target.id + "?controls=1&showinfo=1&rel=0");
     });
-}
-
-$(window).load(function () {
-
-    windowWidth = $(window).width();
-
-    $(this).alignElementsSameHeight();
-    //pictureZoom();
 });
-$(window).resize(function () {
 
-    newWindowWidth = $(window).width();
 
-    if (windowWidth !== newWindowWidth) {
-  setTimeout(function () {
-      $(this).alignElementsSameHeight();
-      //fullScreenContainer();
-      //pictureZoom();
-  }, 205);
-  windowWidth = newWindowWidth;
-    }
 
-});
